cbf767f435cbb0cb4cf71f74de231e0df9e1f4c7
apadarm@gmail.com <apadarm@gmail.com@75dce9a6-304d-4b60-b6a5-8d569f0a142e>
2014-02-27 12:15:02 +0000

    Primer branch

---
+++ branches/Alejandro/SimpleJavaProject/src/net/enalfa/java/simple/ClasePuntoDeEntrada.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.enalfa.java.simple;
+
+/**
+ * @author usuario
+ *
+ */
+public class ClasePuntoDeEntrada {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("How do you do?");
+
+	}
+
+}
