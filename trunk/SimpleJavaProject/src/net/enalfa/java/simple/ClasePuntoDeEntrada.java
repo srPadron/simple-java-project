fc47e99cced078773682d42dc5e8de53865eff9f
apadarm@gmail.com <apadarm@gmail.com@75dce9a6-304d-4b60-b6a5-8d569f0a142e>
2014-02-27 11:56:44 +0000

    Initial commit.

---
+++ trunk/SimpleJavaProject/src/net/enalfa/java/simple/ClasePuntoDeEntrada.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.enalfa.java.simple;
+
+/**
+ * @author usuario
+ *
+ */
+public class ClasePuntoDeEntrada {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
