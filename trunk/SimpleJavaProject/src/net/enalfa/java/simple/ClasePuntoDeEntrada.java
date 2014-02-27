a93f06124403612bb7672fe9b59c025f561bc1f0
apadarm@gmail.com <apadarm@gmail.com@75dce9a6-304d-4b60-b6a5-8d569f0a142e>
2014-02-27 13:32:16 +0000

    Commit trunk modif branch.

--- trunk/SimpleJavaProject/src/net/enalfa/java/simple/ClasePuntoDeEntrada.java
+++ trunk/SimpleJavaProject/src/net/enalfa/java/simple/ClasePuntoDeEntrada.java
@@ -14,7 +14,7 @@ public class ClasePuntoDeEntrada {
 	 */
 	public static void main(String[] args) {
 		System.out.println("How do you do?");
-
+		System.out.println("How does he do at the branches vilcvhes?");
 	}
 
 }
