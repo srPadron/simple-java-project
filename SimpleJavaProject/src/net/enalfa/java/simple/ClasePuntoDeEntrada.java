f8dbeac1007a6b6475891712d5ae66bc21a420db
apadarm@gmail.com <apadarm@gmail.com@75dce9a6-304d-4b60-b6a5-8d569f0a142e>
2014-02-27 12:00:22 +0000

    Primera RELEASE.

--- SimpleJavaProject/src/net/enalfa/java/simple/ClasePuntoDeEntrada.java
+++ SimpleJavaProject/src/net/enalfa/java/simple/ClasePuntoDeEntrada.java
@@ -13,7 +13,7 @@ public class ClasePuntoDeEntrada {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("How do you do?");
 
 	}
 
