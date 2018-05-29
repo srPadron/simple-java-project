63ff55de3d3c8f80ce716bd62d27359eb9828f3f
USERSAD\apadrona <apadrona@everis.com>
2018-05-29 12:54:32 +0200

    modificaciones

--- src/main/java/com/alejandropadron/taller/entrypoint/Archivos.java
+++ src/main/java/com/alejandropadron/taller/entrypoint/Archivos.java
@@ -4,14 +4,14 @@ import java.io.File;
 
 public class Archivos {
 
-	public static void main(String[] args) {
-		File f = new File("src/main/resources");
-		System.out.println("is File?: " + f.isFile());
-		System.out.println("is Direc?: " + f.isDirectory());
+    public static void main(final String[] args) {
+	File f = new File(".");
+	System.out.println("is File?: " + f.isFile());
+	System.out.println("is Direc?: " + f.isDirectory());
 
-		for (File fd : f.listFiles()) {
-			System.out.println("---> " + fd.toString() + " | Path" + fd.getPath() + " |Parent" + fd.getParent());
-		}
-
+	for (File fd : f.listFiles()) {
+	    System.out.println("---> " + fd.toString() + " | Path" + fd.getPath() + " |Parent" + fd.getParent());
 	}
+
+    }
 }
